78b9834b5dec6f8322548a7dbfb8f8e6f677b6ff
Scorsiro <tom.munier@gmail.com>
2016-06-01 02:21:05 +0200

    pushfinal

--- MUNIER_ADLI.sql
+++ MUNIER_ADLI.sql
@@ -1,3 +1,109 @@
+-- MUNIER Tom, ADLI Thafsouth
+
+-- M3106C : Le Seigneur Des Anneaux - TP Noté
+
+BEGIN;
+
+-- Schéma de la base de données
+
+CREATE TABLE RPERS (
+   nomPers TEXT NOT NULL PRIMARY KEY,
+   nomType TEXT,
+   AnNais SMALLINT
+   );
+
+CREATE TABLE RTYPE (
+   nomType TEXT NOT NULL PRIMARY KEY,
+   tailleMoy FLOAT,
+   imberbe BOOLEAN
+   /*FOREIGN KEY (nomType) REFERENCES RPERS(nomType)*/
+   );
+
+ CREATE TABLE RCHAP (
+   numChap SMALLINT NOT NULL,
+   numLivre TEXT NOT NULL,
+   titre TEXT,
+   PRIMARY KEY (numChap, numLivre)
+   );
+
+ CREATE TABLE RCAR (
+   nomPers TEXT NOT NULL REFERENCES RPERS(nomPers) ON DELETE CASCADE,
+   numChap SMALLINT NOT NULL,
+   numLivre TEXT NOT NULL,
+   traitCar TEXT NOT NULL,
+   coefCar FLOAT constraint coef_constraint check(coefCar between 0 and 1),
+   FOREIGN KEY (numChap, numLivre) REFERENCES RCHAP(numChap, numLivre) ON DELETE CASCADE,
+   PRIMARY KEY (nomPers, numChap, numLivre, traitCar)
+   );
+
+-- Insertions
+INSERT INTO RPERS VALUES ('Frodon', 'hobbit', 2968);
+INSERT INTO RPERS VALUES ('Gimli', 'nain', 2879);
+INSERT INTO RPERS VALUES ('Gandalf', 'Istar', 314);
+INSERT INTO RPERS VALUES ('Elrond', 'Elfe', 317);
+INSERT INTO RPERS VALUES ('Galadriel', 'Elfe', 323);
+INSERT INTO RPERS VALUES ('Legolas', 'Elfe', 323);
+INSERT INTO RPERS VALUES ('Aragorn II', 'humain', 2931);
+INSERT INTO RPERS VALUES ('Eorl', 'humain', 2485);
+INSERT INTO RPERS VALUES ('Boromir', 'humain', 2978);
+INSERT INTO RPERS VALUES ('Sméagol', 'hobbit', 2440);
+INSERT INTO RPERS VALUES ('Fléau de Durin', 'balrog', 112);
+
+
+INSERT INTO RTYPE VALUES ('hobbit', 90, 'no');
+INSERT INTO RTYPE VALUES ('nain', 200, 'no');
+INSERT INTO RTYPE VALUES ('Istar', 150, 'no');
+INSERT INTO RTYPE VALUES ('Elfe', 100, 'yes');
+INSERT INTO RTYPE VALUES ('humain', 222, 'no');
+INSERT INTO RTYPE VALUES ('balrog', 9000, 'yes');
+
+
+INSERT INTO RCHAP VALUES (12, 1, 'Fuite vers le gué');
+INSERT INTO RCHAP VALUES (1, 1, 'Une réception depuis longtemps attendue');
+INSERT INTO RCHAP VALUES ( 17, 1, 'Le Pont de Khazad-Dum');
+INSERT INTO RCHAP VALUES (22, 1, 'La dissolution de la Communauté');
+INSERT INTO RCHAP VALUES (12, 2, 'L’apprivoisement de Sméagol');
+INSERT INTO RCHAP VALUES (14, 2, 'La Porte Noire est fermée');
+INSERT INTO RCHAP VALUES (20, 2, 'L’antre d’Arachne');
+INSERT INTO RCHAP VALUES (4, 3, 'Le siège de Gondor');
+INSERT INTO RCHAP VALUES (10, 3, 'La Porte Noire s’ouvre');
+INSERT INTO RCHAP VALUES (13, 3, 'La Montagne du Destin');
+
+
+INSERT INTO RCAR VALUES ('Frodon',12,1,'courageux',0.95);
+INSERT INTO RCAR VALUES ('Frodon',1,1,'joyeux', 0.8);
+INSERT INTO RCAR VALUES ('Frodon',1,1,'impatient',0.7);
+INSERT INTO RCAR VALUES ('Frodon',17,1,'triste',0.5);
+INSERT INTO RCAR VALUES ('Frodon',17,1,'peureux',0.6);
+INSERT INTO RCAR VALUES ('Frodon',22,1,'triste',0.85);
+INSERT INTO RCAR VALUES ('Frodon',12,2,'sagace',0.75);
+INSERT INTO RCAR VALUES ('Frodon',14,2,'découragé',0.55);
+INSERT INTO RCAR VALUES ('Frodon',20,2,'effrayé',0.65);
+INSERT INTO RCAR VALUES ('Frodon',4,3,'impatient',0.45);
+INSERT INTO RCAR VALUES ('Frodon',10,3,'souffrant',0.95);
+INSERT INTO RCAR VALUES ('Frodon',13,3,'agonisant',0.85);
+INSERT INTO RCAR VALUES ('Gimli',22,1,'impétueux',0.99);
+INSERT INTO RCAR VALUES ('Gimli',17,1,'courageux',0.97);
+INSERT INTO RCAR VALUES ('Gimli',4,3,'téméraire',0.96);
+INSERT INTO RCAR VALUES ('Gandalf',12,1,'perspicace',0.80);
+INSERT INTO RCAR VALUES ('Gandalf', 17, 1,'téméraire',0.80);
+INSERT INTO RCAR VALUES ('Gandalf',4,3,'courageux',0.80);
+INSERT INTO RCAR VALUES ('Elrond',22,1,'sage',0.80);
+INSERT INTO RCAR VALUES ('Elrond',12, 2,'paisible',0.80);
+INSERT INTO RCAR VALUES ('Galadriel',12,2,'paisible',0.75);
+INSERT INTO RCAR VALUES ('Legolas',12,2,'paisible',0.78);
+INSERT INTO RCAR VALUES ('Legolas',4,3,'enthousiaste',0.78);
+INSERT INTO RCAR VALUES ('Aragorn II',22,1,'courageux',0.93);
+INSERT INTO RCAR VALUES ('Aragorn II', 4,3,'angoissé', 0.93);
+INSERT INTO RCAR VALUES ('Eorl',12,2,'fier', 0.93);
+INSERT INTO RCAR VALUES ('Boromir',22,1,'fou', 0.93);
+INSERT INTO RCAR VALUES ('Sméagol',13,3,'maladroit',0.5);
+INSERT INTO RCAR VALUES ('Fléau de Durin',17,1,'peureux', 0.5);
+
+
+COMMIT;
+
+
 -- Partie 2 : Optimisation
 
 -- Question 3 :
@@ -199,33 +305,7 @@ nompers     | nomtype | annais | taillemoy | imberbe |   traitcar   | coefcar | 
 ----------------+---------+--------+-----------+---------+--------------+---------+---------+----------+-----------------------------------------
 Frodon         | hobbit  |   2968 |        90 | f       | courageux    |    0.95 |      12 | 1        | Fuite vers le gué
 Frodon         | hobbit  |   2968 |        90 | f       | joyeux       |     0.8 |       1 | 1        | Une réception depuis longtemps attendue
-Frodon         | hobbit  |   2968 |        90 | f       | impatient    |     0.7 |       1 | 1        | Une réception depuis longtemps attendue
-Frodon         | hobbit  |   2968 |        90 | f       | triste       |     0.5 |      17 | 1        | Le Pont de Khazad-Dum
-Frodon         | hobbit  |   2968 |        90 | f       | peureux      |     0.6 |      17 | 1        | Le Pont de Khazad-Dum
-Frodon         | hobbit  |   2968 |        90 | f       | triste       |    0.85 |      22 | 1        | La dissolution de la Communauté
-Frodon         | hobbit  |   2968 |        90 | f       | sagace       |    0.75 |      12 | 2        | L’apprivoisement de Sméagol
-Frodon         | hobbit  |   2968 |        90 | f       | découragé    |    0.55 |      14 | 2        | La Porte Noire est fermée
-Frodon         | hobbit  |   2968 |        90 | f       | effrayé      |    0.65 |      20 | 2        | L’antre d’Arachne
-Frodon         | hobbit  |   2968 |        90 | f       | impatient    |    0.45 |       4 | 3        | Le siège de Gondor
-Frodon         | hobbit  |   2968 |        90 | f       | souffrant    |    0.95 |      10 | 3        | La Porte Noire s’ouvre
-Frodon         | hobbit  |   2968 |        90 | f       | agonisant    |    0.85 |      13 | 3        | La Montagne du Destin
-Gimli          | nain    |   2879 |       200 | f       | impétueux    |    0.99 |      22 | 1        | La dissolution de la Communauté
-Gimli          | nain    |   2879 |       200 | f       | courageux    |    0.97 |      17 | 1        | Le Pont de Khazad-Dum
-Gimli          | nain    |   2879 |       200 | f       | téméraire    |    0.96 |       4 | 3        | Le siège de Gondor
-Gandalf        | Istar   |    314 |       150 | f       | perspicace   |     0.8 |      12 | 1        | Fuite vers le gué
-Gandalf        | Istar   |    314 |       150 | f       | téméraire    |     0.8 |      17 | 1        | Le Pont de Khazad-Dum
-Gandalf        | Istar   |    314 |       150 | f       | courageux    |     0.8 |       4 | 3        | Le siège de Gondor
-Elrond         | Elfe    |    317 |       100 | t       | sage         |     0.8 |      22 | 1        | La dissolution de la Communauté
-Elrond         | Elfe    |    317 |       100 | t       | paisible     |     0.8 |      12 | 2        | L’apprivoisement de Sméagol
-Galadriel      | Elfe    |    323 |        95 | t       | paisible     |    0.75 |      12 | 2        | L’apprivoisement de Sméagol
-Legolas        | Elfe    |    323 |       110 | t       | paisible     |    0.78 |      12 | 2        | L’apprivoisement de Sméagol
-Legolas        | Elfe    |    323 |       110 | t       | enthousiaste |    0.78 |       4 | 3        | Le siège de Gondor
-Aragorn II     | humain  |   2931 |       222 | f       | courageux    |    0.93 |      22 | 1        | La dissolution de la Communauté
-Aragorn II     | humain  |   2931 |       222 | f       | angoissé     |    0.93 |       4 | 3        | Le siège de Gondor
-Eorl           | humain  |   2485 |       212 | f       | fier         |    0.93 |      12 | 2        | L’apprivoisement de Sméagol
-Boromir        | humain  |   2978 |       200 | f       | fou          |    0.93 |      22 | 1        | La dissolution de la Communauté
-Sméagol        | hobbit  |   2440 |        45 | f       | maladroit    |     0.5 |      13 | 3        | La Montagne du Destin
-Fléau de Durin | balrog  |    112 |      9000 | t       | peureux      |     0.5 |      17 | 1        | Le Pont de Khazad-Dum
+...
 perso1         | hobbit  |   2675 |        90 | f       | lâche        |    0.82 |      12 | 2        | L’apprivoisement de Sméagol
 (30 rows)
 */
@@ -276,33 +356,7 @@ nompers     | numchap | numlivre |   traitcar   | coefcar
 ----------------+---------+----------+--------------+---------
 Frodon         |      12 | 1        | courageux    |    0.95
 Frodon         |       1 | 1        | joyeux       |     0.8
-Frodon         |       1 | 1        | impatient    |     0.7
-Frodon         |      17 | 1        | triste       |     0.5
-Frodon         |      17 | 1        | peureux      |     0.6
-Frodon         |      22 | 1        | triste       |    0.85
-Frodon         |      12 | 2        | sagace       |    0.75
-Frodon         |      14 | 2        | découragé    |    0.55
-Frodon         |      20 | 2        | effrayé      |    0.65
-Frodon         |       4 | 3        | impatient    |    0.45
-Frodon         |      10 | 3        | souffrant    |    0.95
-Frodon         |      13 | 3        | agonisant    |    0.85
-Gimli          |      22 | 1        | impétueux    |    0.99
-Gimli          |      17 | 1        | courageux    |    0.97
-Gimli          |       4 | 3        | téméraire    |    0.96
-Gandalf        |      12 | 1        | perspicace   |     0.8
-Gandalf        |      17 | 1        | téméraire    |     0.8
-Gandalf        |       4 | 3        | courageux    |     0.8
-Elrond         |      22 | 1        | sage         |     0.8
-Elrond         |      12 | 2        | paisible     |     0.8
-Galadriel      |      12 | 2        | paisible     |    0.75
-Legolas        |      12 | 2        | paisible     |    0.78
-Legolas        |       4 | 3        | enthousiaste |    0.78
-Aragorn II     |      22 | 1        | courageux    |    0.93
-Aragorn II     |       4 | 3        | angoissé     |    0.93
-Eorl           |      12 | 2        | fier         |    0.93
-Boromir        |      22 | 1        | fou          |    0.93
-Sméagol        |      13 | 3        | maladroit    |     0.5
-Fléau de Durin |      17 | 1        | peureux      |     0.5
+...
 perso1         |      12 | 2        | lâche        |    0.82
 (30 rows)
 */
@@ -425,86 +479,92 @@ EXPLAIN DELETE FROM RPERS WHERE nomPers in (select nomPers from RCAR where numLi
 */
 DELETE FROM RPERS WHERE nomPers in (select nomPers from RCAR where numLivre = '2') ;
 --DELETE 6
--- Partie 3 : Transactions
--- user1
 
---Question 1
+
+
+-- Partie 3 : Transactions
+
+-- Question 1
 BEGIN;
   --Insertion d'un livre
-  INSERT INTO RCHAP VALUES (1,0,"Bilbo le hobbit");
+  INSERT INTO RCHAP VALUES (1,'0','Bilbo le hobbit');
 
   --Insertion de nouveaux personnages
 
-  INSERT INTO RCAR VALUES ('perso2',1, 0, 'lâche', 0.82);
   INSERT INTO RPERS VALUES ('perso2', 'hobbit', 2675);
+  INSERT INTO RCAR VALUES ('perso2',1, 0, 'lâche', 0.82);
 
-
-  INSERT INTO RCAR VALUES ('perso3',1, 0, 'courageux', 0.69);
   INSERT INTO RPERS VALUES ('perso3', 'humain', 2665);
+  INSERT INTO RCAR VALUES ('perso3',1, 0, 'courageux', 0.69);
 
-  INSERT INTO RCAR VALUES ('perso4',1, 0, 'peureux', 0.77);
   INSERT INTO RPERS VALUES ('perso4', 'Elfe', 2684);
+  INSERT INTO RCAR VALUES ('perso4',1, 0, 'peureux', 0.77);
 
 COMMIT;
 
 
---Question 2 : Illustrer un problème de lecture impropre
+-- Question 2 : Illustrer un problème de lecture impropre
 
 --user1
 BEGIN;
 --user2
 BEGIN;
 --user1
-UPDATE RPERS SET annais=5555 WHERE nomPers='Frodon';
+UPDATE RPERS SET annais=5555 WHERE nomPers='Gimli';
 --user2
-SELECT annais FROM RPERS where nomPers ='Frodon' ;
+SELECT annais FROM RPERS where nomPers ='Gimli' ;
 --user1
 ROLLBACK;
 --user2
 COMMIT;
 
---Question 3 : Illustrer un problème de mise à jour
+
+-- Question 3 : Illustrer un problème de mise à jour
 
 --user1
 BEGIN;
 --user2
 BEGIN;
 --user1
-UPDATE RPERS SET annais=5555 WHERE nomPers='Frodon';
+UPDATE RPERS SET annais=5555 WHERE nomPers='Gimli';
 --user2
-UPDATE RPERS SET annais=6666 WHERE nomPers='Frodon' ;
+UPDATE RPERS SET annais=6666 WHERE nomPers='Gimli' ;
 --user2
 COMMIT;
 --user1
-SELECT annais FROM RPERS where nomPers ='Frodon' ;
+SELECT annais FROM RPERS where nomPers ='Gimli' ;
 COMMIT;
 
---Question4 : Illustrer un problème de lecture non reproductible
 
---user2
-BEGIN;
-SELECT annais FROM RPERS where nomPers ='Frodon' ;
+-- Question 4 : Illustrer un problème de lecture non reproductible
+
 --user1
 BEGIN;
-SELECT annais FROM RPERS where nomPers ='Frodon' ;
+SELECT annais FROM RPERS where nomPers ='Gimli' ;
 --user2
-UPDATE RPERS SET annais=5555 WHERE nomPers='Frodon';
+BEGIN;
+UPDATE RPERS SET annais=5555 WHERE nomPers='Gimli';
 COMMIT;
 --user1
-SELECT annais FROM RPERS where nomPers ='Frodon';
-
+SELECT annais FROM RPERS where nomPers ='Gimli';
 COMMIT;
 
---Question5 : Illustrer un problème d'interblocage
+
+-- Question 5 : Illustrer un problème d'interblocage
+
 --user1
-LOCK(RPers);
-SELECT annais FROM RPERS where nomPers ='Frodon' ;
+BEGIN;
+LOCK RPers;
+SELECT annais FROM RPERS where nomPers ='Gimli' ;
 --user2
-LOCK(RTYPE);
+BEGIN;
+LOCK RTYPE;
 SELECT * FROM RTYPE ;
 --user1
-/*En attente pour que user2 libère RTYPE*/
+LOCK RTYPE;
+/*En attente que user2 libère RTYPE*/
 SELECT * FROM RTYPE ;
 --user2
-/*En attente pour que user1 libère RPERS*/
+LOCK RPERS;
+/*En attente que user1 libère RPERS*/
 SELECT annais FROM RPERS where nomPers ='Gimli' ;
